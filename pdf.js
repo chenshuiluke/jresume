53977b3faefbac70ac40db8f7d9f854b4d8fc139
Luke Chen Shui <chenshuiluke@gmail.com>
2017-01-07 03:53:06 -0500

    Created a basic phantomjs script that allows the user to convert the web resume to pdf.

--- pdf.js
+++ pdf.js
@@ -1,2 +1,37 @@
 console.log("Printing PDF");
-phantom.exit();
+var system = require('system');
+console.log("Commandline arguments: " + system.args);
+console.log("Number of commandline arguments: " + system.args.length);
+if(system.args.length < 3){
+	console.log("Please supply the input html resume and the output pdf file.");
+	phantom.exit();
+}
+else{
+	console.log("You have enough arguments!");
+	var page = require('webpage').create();
+	var htmlFile = system.args[1];
+	var outputPdfFile = system.args[2];
+
+	page.paperSize = {
+		format: 'letter',
+		orientation: 'portrait',
+		margin: '1cm'
+	};
+
+
+
+	page.open(htmlFile, function(status){
+		if(status !== 'success'){
+			console.log("Failed to load page!");
+			phantom.exit();
+		}
+		else{
+			console.log("Successfully loaded page!");
+			page.render(outputPdfFile, {
+				format: "pdf",
+				quality: "100"
+			});
+			phantom.exit();
+		}
+	});
+}
