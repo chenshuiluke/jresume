7a2e610a05a3181cca8a6131637e29a994be9592
Luke Chen Shui <chenshuiluke@gmail.com>
2017-01-22 18:31:43 -0500

    Will now return a zipfile containing everything needed for the web resume.

--- src/main/java/com/lukechenshui/jresume/Main.java
+++ src/main/java/com/lukechenshui/jresume/Main.java
@@ -12,8 +12,11 @@ import com.lukechenshui.jresume.resume.items.work.VolunteerWork;
 import com.lukechenshui.jresume.themes.BaseTheme;
 import com.lukechenshui.jresume.themes.BasicExampleTheme;
 import com.lukechenshui.jresume.themes.DefaultTheme;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.model.ZipParameters;
 import org.w3c.tidy.Tidy;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
@@ -25,17 +28,37 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
-        registerThemes();
-        Config config = new Config();
-        new JCommander(config, args);
-        //createExample();
+        try {
+            registerThemes();
+            Config config = new Config();
+            new JCommander(config, args);
+            //createExample();
 
-        if (Config.serverMode) {
-            startListeningAsServer();
-        } else {
-            generateWebResumeFromJSONFile();
+            if (Config.serverMode) {
+                startListeningAsServer();
+            } else {
+                generateWebResumeAndWriteIt(null);
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
         }
+
         //createExample();
+    }
+
+    private static File generateWebResumeAndWriteIt(String json) throws Exception {
+        if (json == null) {
+            json = readJSONFromFile();
+        }
+        String html = generateWebResumeFromJSON(json);
+        File location = Runtime.getOutputHtmlFile();
+        FileWriter writer = new FileWriter(location, false);
+        writer.write(html);
+        //System.out.println(html);
+
+        System.out.println("Success! You can find your resume at " + Runtime.getOutputHtmlFile().getAbsolutePath());
+        writer.close();
+        return location.getParentFile();
     }
 
     public static void registerThemes() {
@@ -69,6 +92,7 @@ public class Main {
         }
         catch (Exception exc){
             exc.printStackTrace();
+            stop();
         }
     }
 
@@ -83,35 +107,46 @@ public class Main {
             Runtime.unzipResourceZip(tempFile.getAbsolutePath());
     }
 
-    private static void startListeningAsServer() {
-        threadPool(3);
+    private static void startListeningAsServer() throws Exception {
+        threadPool(4);
         port(Config.getServerPort());
         post("/webresume", (request, response) -> {
-            return "Hello";
+
+            File location = generateWebResumeAndWriteIt(request.body());
+            File outputZipFile = File.createTempFile("jresume", ".tmp");
+            if (outputZipFile.exists()) {
+                outputZipFile.delete();
+            }
+            outputZipFile.deleteOnExit();
+            ZipFile zipFile = new ZipFile(outputZipFile);
+            zipFile.createZipFileFromFolder(location, new ZipParameters(), false, 0);
+            HttpServletResponse rawResponse = response.raw();
+            rawResponse.setContentType("application/octet-stream");
+            rawResponse.setHeader("Content-Disposition", "attachment; filename=resume.zip");
+            OutputStream out = rawResponse.getOutputStream();
+            out.write(Files.readAllBytes(outputZipFile.toPath()));
+
+            return rawResponse;
         });
         get("/", (request, response) -> {
             return "Welcome to JResume!";
         });
+
+        exception(Exception.class, (exception, request, response) -> {
+            exception.printStackTrace();
+            stop();
+            System.exit(1);
+        });
     }
 
-    private static void generateWebResumeFromJSONFile() {
-        String jsonResumePath = Config.getInputFileName();
+    private static String generateWebResumeFromJSON(String json) throws Exception {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String json = "";
-
-        try {
             copyResourcesZip();
             if (!Files.exists(Paths.get("output"))) {
                 Files.createDirectory(Paths.get("output"));
             }
 
-            Scanner reader = new Scanner(new File(jsonResumePath));
-            FileWriter writer = new FileWriter(Runtime.getOutputHtmlFile(), false);
-            while (reader.hasNextLine()) {
-                json += reader.nextLine();
-                json += "\n";
-            }
-            reader.close();
+
             //System.out.println(json);
             Resume resume = gson.fromJson(json, Resume.class);
 
@@ -122,24 +157,35 @@ public class Main {
             BaseTheme theme = Config.getThemeHashMap().get(Config.getThemeName());
             String html = theme.generate(resume);
 
-            Tidy tidy = new Tidy();
-            tidy.setIndentContent(true);
-            tidy.setShowWarnings(false);
-            tidy.setQuiet(true);
-            tidy.setTrimEmptyElements(false);
+        html = prettyPrintHTML(html);
+        return html;
+    }
 
-            StringReader htmlStringReader = new StringReader(html);
-            StringWriter htmlStringWriter = new StringWriter();
-            tidy.parseDOM(htmlStringReader, htmlStringWriter);
-            html = htmlStringWriter.toString();
-            writer.write(html);
+    private static String prettyPrintHTML(String html) {
+        String prettyHTML;
+        Tidy tidy = new Tidy();
+        tidy.setIndentContent(true);
+        tidy.setShowWarnings(false);
+        tidy.setQuiet(true);
+        tidy.setTrimEmptyElements(false);
 
-            //System.out.println(html);
+        StringReader htmlStringReader = new StringReader(html);
+        StringWriter htmlStringWriter = new StringWriter();
+        tidy.parseDOM(htmlStringReader, htmlStringWriter);
+        prettyHTML = htmlStringWriter.toString();
+        return prettyHTML;
+    }
 
-            System.out.println("Success! You can find your resume at " + Runtime.getOutputHtmlFile().getAbsolutePath());
-            writer.close();
-        } catch (Exception exc) {
-            exc.printStackTrace();
+    private static String readJSONFromFile() throws Exception {
+        String jsonResumePath = Config.getInputFileName();
+        String json = "";
+        Scanner reader = new Scanner(new File(jsonResumePath));
+
+        while (reader.hasNextLine()) {
+            json += reader.nextLine();
+            json += "\n";
         }
+        reader.close();
+        return json;
     }
 }
