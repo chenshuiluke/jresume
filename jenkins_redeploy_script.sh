d757d8d19d165be0ce3ab4eda857780668f2f862
Luke Chen Shui <chenshuiluke@gmail.com>
2017-01-27 13:41:54 -0500

    Changed Jenkins logging directory

--- jenkins_redeploy_script.sh
+++ jenkins_redeploy_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pkill -f jresume-1.0-SNAPSHOT-jar-with-dependencies.jar
 cd target
-nohup java -jar jresume-1.0-SNAPSHOT-jar-with-dependencies.jar -i bleh --server-mode --ssl-mode --log-file /jresume_logs &
-#java -jar jresume-1.0-SNAPSHOT-jar-with-dependencies.jar -i bleh --server-mode --ssl-mode --log-file /jresume_logs
+nohup java -jar jresume-1.0-SNAPSHOT-jar-with-dependencies.jar -i bleh --server-mode --ssl-mode --log-file /jresume_logs/logs.txt &
+#java -jar jresume-1.0-SNAPSHOT-jar-with-dependencies.jar -i bleh --server-mode --ssl-mode --log-file /jresume_logs/logs.txt
