29a8006af19f3e30d02e376e82bdd8a27062711d
Luke Chen Shui <chenshuiluke@gmail.com>
2017-02-28 12:54:13 -0500

    Started using Freemarker templating. Made good progress on converting the example theme.

--- src/main/java/com/lukechenshui/jresume/Config.java
+++ src/main/java/com/lukechenshui/jresume/Config.java
@@ -1,135 +1,140 @@
 package com.lukechenshui.jresume;
 
 import com.beust.jcommander.Parameter;
-import com.lukechenshui.jresume.themes.BaseTheme;
 import com.lukechenshui.jresume.validators.FileLocationValidator;
-import com.lukechenshui.jresume.validators.ThemeNameValidator;
 
 import java.io.File;
-import java.util.HashMap;
 
 /**
  * Created by luke on 1/1/17.
  */
 public class Config {
+    public Config() {
+    }
+
     @Parameter(names = {"--output", "-o"}, description = "The output directory for the generated resume.")
-    static String outputDirectory = "output";
-    @Parameter(names = {"--theme", "-t"}, description = "The theme to use.", validateWith = ThemeNameValidator.class)
-    static String themeName = "default";
+    String outputDirectory = "output";
+    @Parameter(names = {"--theme", "-t"}, description = "The theme to use.")
+    String themeName = "default";
     @Parameter(names = {"--server-mode", "-s"}, description = "Launches JResume in server mode.")
-    static boolean serverMode = false;
+    boolean serverMode = false;
     @Parameter(names = {"--server-port", "-sp"}, description = "The port that JResume will listen to when in server mode.")
-    static int serverPort = 8080;
+    int serverPort = 8080;
     @Parameter(names = {"--ssl-mode", "-sm"}, description = "Makes JResume use ssl while in server mode. " +
             "Uses the jresume_keystore_location and jresume_keystore_password for configuring which keystore and password to use." +
             "See https://maximilian-boehm.com/hp2121/Create-a-Java-Keystore-JKS-from-Let-s-Encrypt-Certificates.htm for information on how to generate a keystore" +
             "from a letsencrypt certificate.")
-    static boolean sslMode = false;
+    boolean sslMode = false;
     @Parameter(names = {"--max-threads", "-mt"}, description = "The maximum number of spark threads allowed.")
-    static int maxThreads = Integer.MAX_VALUE;
+    int maxThreads = Integer.MAX_VALUE;
     /*If the serverMode parameter is present then --input can be set to anything as it's only important if
       JResume is not running as a server
     */
     @Parameter(names = {"--input", "-i"}, description = "The location of the input file.", required = true)
-    static String inputFileName;
+    String inputFileName;
 
     @Parameter(names = {"--log-file", "-l"}, description = "The location of the log file", validateWith = FileLocationValidator.class)
-    static String logFile;
+    String logFile;
 
     @Parameter(names = {"--resource-directory", "-rd"}, description = "The subdirectory where the CSS/JS etc resources will be within the zip file.")
-    static String resourceDirectory = "resources";
+    String resourceDirectory = "resources";
 
     @Parameter(names = {"--show-trackable-information", "-st"}, description = "Shows trackable information such as your address, phone number, etc.")
-    static boolean showTrackable = false;
+    boolean showTrackable = false;
     /*
     This zipfile will be created at startup when the jresume is running in server mode. Then, it will
     be copied to each request's output directory and the generated web resume will be added to the copy and
     served to the user.
      */
-    static File serverInitialResourceZip = new File("data/webresume-resources.zip");
-
-    private static HashMap<String, BaseTheme> themeHashMap = new HashMap<String, BaseTheme>();
+    File serverInitialResourceZip = new File("data/webresume-resources.zip");
 
 
-    public static void addTheme(Object theme) {
-        if (theme instanceof BaseTheme) {
-            BaseTheme themeObj = (BaseTheme) theme;
-            themeHashMap.put(themeObj.getThemeName(), themeObj);
-        } else {
-            throw new IllegalArgumentException("Object passed to addTheme is not instance or decendant of BaseTheme.");
-        }
 
-    }
-
-    public static HashMap<String, BaseTheme> getThemeHashMap() {
-        return themeHashMap;
-    }
-
-    public static void setThemeHashMap(HashMap<String, BaseTheme> themeHashMap) {
-        Config.themeHashMap = themeHashMap;
-    }
-
-    public static String getOutputDirectory() {
+    public String getOutputDirectory() {
         return outputDirectory;
     }
 
-    public static void setOutputDirectory(String outputDirectory) {
-        Config.outputDirectory = outputDirectory;
+    public void setOutputDirectory(String outputDirectory) {
+        this.outputDirectory = outputDirectory;
     }
 
-    public static String getThemeName() {
+    public String getThemeName() {
         return themeName;
     }
 
-    public static void setThemeName(String themeName) {
-        Config.themeName = themeName;
+    public void setThemeName(String themeName) {
+        this.themeName = themeName;
     }
 
-    public static String getInputFileName() {
+    public String getInputFileName() {
         return inputFileName;
     }
 
-    public static void setInputFileName(String inputFileName) {
-        Config.inputFileName = inputFileName;
+    public void setInputFileName(String inputFileName) {
+        this.inputFileName = inputFileName;
     }
 
-    public static boolean isServerMode() {
+    public boolean isServerMode() {
         return serverMode;
     }
 
-    public static void setServerMode(boolean serverMode) {
-        Config.serverMode = serverMode;
+    public void setServerMode(boolean serverMode) {
+        this.serverMode = serverMode;
     }
 
-    public static int getServerPort() {
+    public int getServerPort() {
         return serverPort;
     }
 
-    public static void setServerPort(int serverPort) {
-        Config.serverPort = serverPort;
+    public void setServerPort(int serverPort) {
+        this.serverPort = serverPort;
     }
 
-    public static int getMaxThreads() {
+    public int getMaxThreads() {
         return maxThreads;
     }
 
-    public static void setMaxThreads(int maxThreads) {
-        Config.maxThreads = maxThreads;
+    public void setMaxThreads(int maxThreads) {
+        this.maxThreads = maxThreads;
     }
 
-    public static String getResourceDirectory() {
+    public String getResourceDirectory() {
         return resourceDirectory;
     }
 
-    public static void setResourceDirectory(String resourceDirectory) {
-        Config.resourceDirectory = resourceDirectory;
+    public void setResourceDirectory(String resourceDirectory) {
+        this.resourceDirectory = resourceDirectory;
     }
 
-    public static boolean isShowTrackable() {
+    public boolean isShowTrackable() {
         return showTrackable;
     }
 
-    public static void setShowTrackable(boolean showTrackable) {
-        Config.showTrackable = showTrackable;
+    public void setShowTrackable(boolean showTrackable) {
+        this.showTrackable = showTrackable;
+    }
+
+    public boolean isSslMode() {
+        return sslMode;
+    }
+
+    public void setSslMode(boolean sslMode) {
+        this.sslMode = sslMode;
+    }
+
+    public String getLogFile() {
+        return logFile;
+    }
+
+    public void setLogFile(String logFile) {
+        this.logFile = logFile;
+    }
+
+    public File getServerInitialResourceZip() {
+        return serverInitialResourceZip;
+    }
+
+    public void setServerInitialResourceZip(File serverInitialResourceZip) {
+        this.serverInitialResourceZip = serverInitialResourceZip;
     }
 }
