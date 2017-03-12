8d085c600a6f04b36d453d93c7e63c6c9ccc24ec
Luke Chen Shui <chenshuiluke@gmail.com>
2017-03-12 14:44:09 -0500

    Updated Jenkins deploy script

--- jenkins_redeploy_script.sh
+++ jenkins_redeploy_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pkill -f jresume-1.0-SNAPSHOT-jar-with-dependencies.jar
-cd target
+cp target/jresume-1.0-SNAPSHOT-jar-with-dependencies.jar .
 nohup java -jar jresume-1.0-SNAPSHOT-jar-with-dependencies.jar -i bleh --server-mode --ssl-mode --log-file /jresume_logs/logs.txt &
 #java -jar jresume-1.0-SNAPSHOT-jar-with-dependencies.jar -i bleh --server-mode --ssl-mode --log-file /jresume_logs/logs.txt
