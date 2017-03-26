1eca1541832f7fc454ff2f855e41bfe5dcb57cb0
Luke Chen Shui <chenshuiluke@gmail.com>
2017-03-26 16:23:34 -0500

    Changed Jenkins redeploy script

--- jenkins_redeploy_script.sh
+++ jenkins_redeploy_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pkill -f jresume-1.0-SNAPSHOT-jar-with-dependencies.jar
 cp target/jresume-1.0-SNAPSHOT-jar-with-dependencies.jar .
-nohup java -jar jresume-1.0-SNAPSHOT-jar-with-dependencies.jar -i bleh --server-mode --ssl-mode --log-file /jresume_logs/logs.txt -sp 7000 &
-#java -jar jresume-1.0-SNAPSHOT-jar-with-dependencies.jar -i bleh --server-mode --ssl-mode --log-file -sp 7000 /jresume_logs/logs.txt
+nohup java -jar jresume-1.0-SNAPSHOT-jar-with-dependencies.jar -i bleh --server-mode --ssl-mode --log-file /jresume_logs/logs.txt -sp 7000 -st &
+#java -jar jresume-1.0-SNAPSHOT-jar-with-dependencies.jar -i bleh --server-mode --ssl-mode --log-file -sp 7000 -st /jresume_logs/logs.txt
