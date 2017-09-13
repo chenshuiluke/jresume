ca1b55a0aa0a9992e7bd067baa67da9942ae3943
Luke Chen Shui <chenshuiluke@gmail.com>
2017-09-13 17:17:16 -0500

    Refactored Code is Now Compilable

---
+++ src/main/java/com/lukechenshui/jresume/Router.java
@@ -0,0 +1,127 @@
+package com.lukechenshui.jresume;
+import static spark.Spark.*;
+import spark.Request;
+import spark.Response;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import com.lukechenshui.jresume.ResumeGenerator.WEBREQUEST_TYPE;
+import com.lukechenshui.jresume.Config;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.apache.commons.io.FileDeleteStrategy;
+import org.apache.commons.io.FileUtils;
+import java.util.Optional;
+import com.lukechenshui.jresume.exceptions.InvalidEnvironmentVariableException;
+import java.util.ArrayList;
+import org.apache.commons.io.FilenameUtils;
+class Router{
+  Config config;
+  Router(Config config){
+    this.config = config;
+  }
+  private void prepareEnvironment() throws IOException{
+    if (Files.exists(Paths.get("data"))) {
+        FileDeleteStrategy.FORCE.delete(new File("data"));
+    }
+
+    Files.createDirectory(Paths.get("data"));
+  }
+  public void initializeRoutes() throws InvalidEnvironmentVariableException{
+    if (config.sslMode) {
+        String keystoreLocation = Optional.ofNullable(System.getenv("jresume_keystore_location")).orElseThrow(
+                () -> new InvalidEnvironmentVariableException("jresume_keystore_location is not set in the environment"));
+
+        String keystorePassword = Optional.ofNullable(System.getenv("jresume_keystore_password")).orElseThrow(
+                () -> new InvalidEnvironmentVariableException("jresume_keystore_password is not set in the environment"));
+        File keystore = new File(keystoreLocation);
+        System.out.println("Keystore location:" + keystore.getAbsolutePath());
+        System.out.println("Keystore exists: " + keystore.exists());
+        System.out.println("Keystore can be read: " + keystore.canRead());
+        System.out.println("Keystore can write: " + keystore.canWrite());
+        System.out.println("Keystore can execute: " + keystore.canExecute());
+        secure(keystoreLocation, keystorePassword, null, null);
+    }
+    post("/webresume/html", "application/json", (request, response) -> {
+      ResumeGenerator generator = new ResumeGenerator(config);
+      return generator.generateResumeInRoute("default", request, response, WEBREQUEST_TYPE.HTML);
+    });
+
+    post("/webresume/pdf", "application/json", (request, response) -> {
+      ResumeGenerator generator = new ResumeGenerator(config);
+      return generator.generateResumeInRoute("default", request, response, WEBREQUEST_TYPE.PDF);
+    });
+
+//        post("/webresume/:theme", (request, response) -> {
+//            String desiredTheme = request.params(":theme");
+//            ArrayList<String> themes = getListOfThemes();
+//            if (themes.contains(desiredTheme)) {
+//                return generateWebResumeInRoute(desiredTheme, request, response, WEBREQUEST_TYPE.HTML);
+//            } else {
+//                response.type("application/json");
+//                response.status(400);
+//
+//                JSONObject responseObj = new JSONObject();
+//                responseObj.put("error", "The theme you have selected does not exist");
+//
+//                return responseObj.toString();
+//            }
+//        });
+
+
+
+    get("/", (request, response) -> {
+        return "Welcome to JResume!";
+    });
+
+    get("/themes", (request, response) -> {
+
+        response.type("application/json");
+        JSONObject responseObj = new JSONObject();
+        JSONArray themeArr = new JSONArray();
+        File themeFolder = new File("themes");
+        for (String themeName : themeFolder.list()) {
+            themeArr.put(FilenameUtils.getBaseName(themeName));
+        }
+        responseObj.put("themes", themeArr);
+        return responseObj.toString();
+    });
+    exception(Exception.class, (exception, request, response) -> {
+        exception.printStackTrace();
+    });
+  }
+
+  private void startListeningAsServer() throws Exception {
+      threadPool(config.getMaxThreads());
+      port(config.getServerPort());
+      enableCORS("*", "POST, GET, OPTIONS, DELETE, PUT", "*");
+  }
+
+  private void enableCORS(final String origin, final String methods, final String headers) {
+
+      options("/*", (request, response) -> {
+
+          String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+          if (accessControlRequestHeaders != null) {
+              response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+          }
+
+          String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+          if (accessControlRequestMethod != null) {
+              response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+          }
+
+          return "OK";
+      });
+
+      before((request, response) -> {
+          response.header("Access-Control-Allow-Origin", origin);
+          response.header("Access-Control-Request-Method", methods);
+          response.header("Access-Control-Allow-Headers", headers);
+          // Note: this may or may not be necessary in your particular application
+          response.type("application/json");
+      });
+  }
+}
--- src/main/java/com/lukechenshui/jresume/RuntimeConfiguration.java
+++ src/main/java/com/lukechenshui/jresume/RuntimeConfiguration.java
@@ -1,64 +1,47 @@
 package com.lukechenshui.jresume;
 
-import net.lingala.zip4j.core.ZipFile;
-import org.apache.commons.io.FileDeleteStrategy;
-import org.apache.commons.io.FileUtils;
+
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 /**
- * Created by luke on 1/1/17.
+ * A per-resume generation request configuration object
  */
-public class Runtime {
+public class RuntimeConfiguration {
+
+
     private  File tempDirectory;
     private File outputDirectory;
     private int id;
     private Config config;
-    private Main.WEBREQUEST_TYPE webrequest_type = Main.WEBREQUEST_TYPE.HTML;
-    public Runtime(File outputDirectory, int id, Config config) {
+    private ResumeGenerator.WEBREQUEST_TYPE webrequest_type = ResumeGenerator.WEBREQUEST_TYPE.HTML;
+    public RuntimeConfiguration(File outputDirectory, int id, Config config) {
         this.outputDirectory = outputDirectory;
         this.id = id;
         this.config = config;
     }
 
-    public Main.WEBREQUEST_TYPE getWebRequestType() {
+    public ResumeGenerator.WEBREQUEST_TYPE getWebRequestType() {
         return webrequest_type;
     }
 
-    public void setWebRequestType(Main.WEBREQUEST_TYPE webrequest_type) {
+    public void setWebRequestType(ResumeGenerator.WEBREQUEST_TYPE webrequest_type) {
         this.webrequest_type = webrequest_type;
     }
 
-    public Runtime(File outputDirectory, int id, Config config, Main.WEBREQUEST_TYPE type) {
+    public RuntimeConfiguration(File outputDirectory, int id, Config config, ResumeGenerator.WEBREQUEST_TYPE type) {
         this(outputDirectory, id, config);
         this.webrequest_type = type;
     }
 
-    public Runtime(String outputDirectoryStr, int id, Config config) {
+    public RuntimeConfiguration(String outputDirectoryStr, int id, Config config) {
         this.outputDirectory = new File(outputDirectoryStr);
         this.id = id;
         this.config = config;
     }
 
-    public  void unzipResourceZip(String file) {
-        try {
-            if (!Files.exists(Paths.get("output"))) {
-                Files.createDirectory(Paths.get("output"));
-            }
-            File resourceDirectory = Paths.get(outputDirectory.getAbsolutePath(), config.getResourceDirectory()).toFile();
-            if (!resourceDirectory.exists()) {
-                resourceDirectory.mkdirs();
-            }
-
-            ZipFile zipFile = new ZipFile(file);
-            zipFile.extractAll(resourceDirectory.getAbsolutePath());
-
-        } catch (Exception exc) {
-            exc.printStackTrace();
-        }
-    }
 
     public  File getResourceDirectory() {
         return Paths.get(outputDirectory.getAbsolutePath(), "/resources").toFile();
