3e332f26fbceb5ba05326d437f50e0ed119f214b
Luke Chen Shui <chenshuiluke@gmail.com>
2017-09-13 01:03:09 -0500

    Added Ability to Download PDF Files

--- src/main/java/com/lukechenshui/jresume/Runtime.java
+++ src/main/java/com/lukechenshui/jresume/Runtime.java
@@ -16,10 +16,24 @@ public class Runtime {
     private File outputDirectory;
     private int id;
     private Config config;
+    private Main.WEBREQUEST_TYPE webrequest_type = Main.WEBREQUEST_TYPE.HTML;
     public Runtime(File outputDirectory, int id, Config config) {
         this.outputDirectory = outputDirectory;
         this.id = id;
         this.config = config;
+    }
+
+    public Main.WEBREQUEST_TYPE getWebRequestType() {
+        return webrequest_type;
+    }
+
+    public void setWebRequestType(Main.WEBREQUEST_TYPE webrequest_type) {
+        this.webrequest_type = webrequest_type;
+    }
+
+    public Runtime(File outputDirectory, int id, Config config, Main.WEBREQUEST_TYPE type) {
+        this(outputDirectory, id, config);
+        this.webrequest_type = type;
     }
 
     public Runtime(String outputDirectoryStr, int id, Config config) {
